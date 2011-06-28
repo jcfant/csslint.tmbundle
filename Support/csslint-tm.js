e58226dc5138af01d8e2ffcb09cb5ac53ee791b7
JC Fant IV <jcfant@gmail.com>
2011-06-28 10:12:15 -0700

    Added new auto update notification for csslint bundle. Changed path to csslint-node.js

--- Support/csslint-tm.js
+++ Support/csslint-tm.js
@@ -1,13 +1,20 @@
-#!/usr/bin/env node
+#!/usr/local/bin/node
 var env = process.env || process.ENV,
 	http = require('https'),
 	fs = require('fs'),
 	sys = require('sys'),
+	port = "443",
 	stdout  = process.stdout,
+	showVersionMessage = false,
+	versionMessage = "There is a new version of cssLint Bundle. Please upgrade soon.",
 	cssHost = "raw.github.com", 
 	cssFile = "csslint-node.js",
-	cssPath = "/stubbornella/csslint/master/build/", 
-	cssLintFile = env.TM_BUNDLE_SUPPORT + "/" + cssFile;
+	cssVersion = "csslint-version",
+	cssTempVersion = "csslint-version.tmp",
+	cssPath = "/stubbornella/csslint/master/release/", 
+	cssLintFile = env.TM_BUNDLE_SUPPORT + "/" + cssFile,
+	cssTMFile = env.TM_BUNDLE_SUPPORT + "/" + cssVersion,
+	cssTempFile = env.TM_BUNDLE_SUPPORT + "/" + cssTempVersion;
 
 
 /*
@@ -28,13 +35,13 @@ function html(s) {
  * Download the latest CSSLint file.
  */
 
-function getLatestJS(callback){
-	var request = http.get({host: cssHost, port: 443, path: cssPath + cssFile}, function(response){
+function getLatest(callback, file, output){
+	var request = http.get({host: cssHost, port: port, path: file}, function(response){
 		if (response.statusCode == 200){
 			response.setEncoding("utf8");
 			var d = "";
 			response.on('data', function(chunk) { d += chunk; });
-			response.on('end', function() { fs.writeFile(cssLintFile, d, callback); });
+			response.on('end', function() { fs.writeFile(output, d, callback); });
 		}else{
 			callback('Bad HTTP Status Code: ' + response.statusCode);
 		}
@@ -43,10 +50,51 @@ function getLatestJS(callback){
 	});
 }
 
+function versionsMatch(tmContents, tempContents, callback){
+	fs.readFile(cssTMFile, function(error, cssTMContents){
+		cssTMContents = String(cssTMContents).replace(/^\s+|\s+$/, '');
+		if (error)
+			callback(false);
+			
+		fs.stat(cssTempFile, function(error, stats){
+			if (error){
+				callback(false);
+			}else{
+				fs.readFile(cssTempFile, function(error, cssTempContents){
+					cssTempContents = String(cssTempContents).replace(/^\s+|\s+$/, '');
+					callback( (cssTMContents != cssTempContents) );
+				});
+			}
+		});
+	});
+}
+
+function versionUpdate(show){
+	showVersionMessage = show;
+	versionUpdate.callback();
+	versionUpdate.callback = undefined;
+}
+
+function version(error) {
+	fs.stat(cssTMFile, function(error, stats){
+		if (error){
+			// just move .tmp to .version
+			fs.readFile(cssTempFile, function(error, contents){
+				fs.writeFile(cssTMFile, contents, function() { 
+					versionsMatch(cssTMFile, cssTempFile, versionUpdate);
+				});
+			});
+		}else{
+			versionsMatch(cssTMFile, cssTempFile, versionUpdate);
+		}
+	});
+}
+
+
 function hasLatestJS(callback){
 	fs.stat(cssLintFile, function(error, stats){
 		if (error){
-			getLatestJS(callback);
+			getLatest(callback, cssPath + cssFile, cssLintFile);
 			return;
 		}
 		
@@ -54,66 +102,99 @@ function hasLatestJS(callback){
 		var mDate = Date.parse(stats.mtime);
 		var lDate = aDate < mDate ? aDate : mDate;
 		if ((Date.now() - lDate) / 1000 / 60 / 60 / 24 >= 7){
-			getLatestJS(callback);
+			getLatest(callback, cssPath + cssFile, cssLintFile);
 		}else{
 			callback();
 		}
 	});
 }
 
-module.exports = function(options) {
-	hasLatestJS(function(error){
-		if (options === undefined)
-			options = {};
+function hasLatestVersion(callback){
+	fs.stat(cssTMFile, function(error, stats){
+		if (error){
+			//sys.puts(error);
+			versionUpdate.callback = callback;
+			getLatest(version, cssPath + cssVersion,  cssTempFile);
+			return;
+		}
+		
+		versionUpdate.callback = callback;
+		fs.stat(cssTempFile, function(error){
+			if (error){
+				getLatest(version, cssPath + cssVersion,  cssTempFile);
+			}
 			
-		var file = env.TM_FILEPATH;
-		var input = fs.readFileSync(file, 'utf8');
-		var CSSLint = require(cssLintFile).CSSLint;
+			var aDate = Date.parse(stats.atime);
+			var mDate = Date.parse(stats.mtime);
+			var lDate = aDate < mDate ? aDate : mDate;
+			if ((Date.now() - lDate) / 1000 / 60 / 60 / 24 >= 7){
+				getLatest(version, cssPath + cssVersion,  cssTempFile);
+			}else{
+				versionsMatch(cssTMFile, cssTempFile, versionUpdate);
+			}
+		});
+	});
+}
 
-		var result = CSSLint.verify(input);
-		var messages = result.messages;
+module.exports = function(options) {
+	// check for latest bundle version
+	hasLatestVersion(function(){
+		// check for latest css lint
+		hasLatestJS(function(error){
+			if (options === undefined)
+				options = {};
+			
+			var file = env.TM_FILEPATH;
+			var input = fs.readFileSync(file, 'utf8');
+			var CSSLint = require(cssLintFile).CSSLint;
+
+			var result = CSSLint.verify(input);
+			var messages = result.messages;
 		
-		var body = "";
+			var body = "";
 		
-		if (messages.length){
-		    if (error){
-			    body += '<div class="cssHeader">' + error + '</div>';
-		    }
-		    body += '<div class="cssHeader">CSSLint</div>';
+			if (messages.length){
+				if (showVersionMessage){
+					body += '<div class="cssHeader">' + versionMessage + '</div>';
+				}
+			    if (error){
+				    body += '<div class="cssHeader">' + error + '</div>';
+			    }
 		
-			if (options.rollup){
-				//rollups at the bottom
-				messages.sort(function(a, b){
-					if (a.rollup){
-						return -1;
-					} else if (b.rollup){
-						return 1;
-					} else {
-						return 0;
-					}
+				if (options.rollup){
+					//rollups at the bottom
+					messages.sort(function(a, b){
+						if (a.rollup){
+							return -1;
+						} else if (b.rollup){
+							return 1;
+						} else {
+							return 0;
+						}
+					});
+				}
+
+				messages.forEach(function(message,i){
+					var link = 'txmt://open?url=file://' + escape(file) + '&line=' + message.line;
+					body += "<div class=\"entry " + message.type + "\"><a href=\"" + link + "\">";
+					body += "<div class=\"header " + message.type + "\">" + message.type + "</div>";
+					body += "<div class=\"message\">" + html(message.message) + "</div>";
+					body += "<div class=\"evidence\">" + html(message.evidence) + "</div>";
+					body += '</a>';
+					body += "</div>";
 				});
 			}
 
-			messages.forEach(function(message,i){
-				var link = 'txmt://open?url=file://' + escape(file) + '&line=' + message.line;
-				body += "<div class=\"entry " + message.type + "\"><a href=\"" + link + "\">";
-				body += "<div class=\"header " + message.type + "\">" + message.type + "</div>";
-				body += "<div class=\"message\">" + html(message.message) + "</div>";
-				body += "<div class=\"evidence\">" + html(message.evidence) + "</div>";
-				body += '</a>';
-				body += "</div>";
-			});
-		}
-
 		
-		/*
-		 * Original version of  writing the error messages to TextMate from jshint.tmbundle
-		 */
-		if (body.length > 0) {
-			fs.readFile(__dirname + '/output.html', 'utf8', function(e, html) {
-				sys.puts(html.replace('{body}', body));
-				process.exit(205); //show_html
-			});
-		}
+			/*
+			 * Original version of  writing the error messages to TextMate from jshint.tmbundle
+			 */
+			if (body.length > 0) {
+				fs.readFile(__dirname + '/output.html', 'utf8', function(e, html) {
+					sys.puts(html.replace('{body}', body));
+					process.exit(205); //show_html
+				});
+			}
+		});
 	});
 };
